71677ae06d0f6ba13950953489243310e1fb02ba
Domantas <domantas.mauruca@gmail.com>
2018-05-17 13:52:21 +0200

    Adding Typescript support

--- packages/lerna-starter-example/src/index.tsx
+++ packages/lerna-starter-example/src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import * as React from 'react';
 
 import DefaultStyledButton from './styles/defaultStyledButton';
 
-const Button = ({ type = 'button', children, onClick }) => (
+const Button = ({ type = 'button', children, onClick }: any) => {
   <DefaultStyledButton type={type} className="button" onClick={onClick}>
     {children}
   </DefaultStyledButton>
-);
+};
 
 export default Button;
